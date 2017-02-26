45b11377b71e0d46f5401f0ddbdc5cd77299fa9a
Tanja Zuser <tanjazuser@gmx.at>
2017-02-26 13:30:59 +0100

    added  if-else made chupa gallery responsive added alert

--- Chupin-Gallery/js/script.js
+++ Chupin-Gallery/js/script.js
@@ -9,16 +9,25 @@ $(document).ready(function() {
 
     $('#previous').on('click', function() {
         var previousChupin = currentChupin - 1;
-        $('#change-chupin').attr('src', 'img/bild00' + previousChupin + '.jpg');
-        currentChupin--;
+        if (previousChupin !== 0) {
+            $('#change-chupin').attr('src', 'img/bild00' + previousChupin + '.jpg');
+            currentChupin--;
+        } else {
+            window.alert('Beginning of Chupas reached');
+        }
+
 
         // if abfrage, falls currentChupin == 1
     });
 
     $('#next').on('click', function() {
-        var previousChupin = currentChupin + 1;
-        $('#change-chupin').attr('src', 'img/bild00' + previousChupin + '.jpg');
-        currentChupin++;
+        var nextChupin = currentChupin + 1;
+        if (nextChupin !== 7) {
+            $('#change-chupin').attr('src', 'img/bild00' + nextChupin + '.jpg');
+            currentChupin++;
+        } else {
+            window.alert('End of Chupas reached');
+        }
 
         // if abfrage, falls currentChupin == 6
     });
